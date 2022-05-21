5ffc1b337e2eab392b53c3c01b0772fe94479680
TAPASH MOLLICK <tapashm42@gmail.com>
2022-05-21 15:25:02 +0530

    updated code
    updated code

--- src/items/items.router.ts
+++ src/items/items.router.ts
@@ -19,32 +19,93 @@ export const itemsRouter = express.Router();
 // GET items
 
 itemsRouter.get("/", async (req: Request, res: Response) => {
-    try {
-      const items: Item[] = await ItemService.findAll();
-  
-      res.status(200).send(JSON.stringify(items));
-    } catch (e: unknown) {
-        if (e instanceof Error) {
+  try {
+    const items: Item[] = await ItemService.findAll();
+
+    res.status(200).send(JSON.stringify(items));
+  } catch (e: unknown) {
+    if (e instanceof Error) {
       res.status(500).send(e.message);
     }
-    }
-  });
+  }
+});
 
 // GET items/:id
 
-// itemsRouter.get("/",async (req: Request, res: Response) => {
+itemsRouter.get("/", async (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id, 10);
+  try {
+    const item: Item = await ItemService.find(id);
 
-//     try {
-//         const item: Item = await ItemService.find()
-//     }
+    if (item) {
+      return res.status(200).send(item);
+    }
 
-    
-// });
+    res.status(404).send(item);
+  }
+  catch (e: unknown) {
+    if (e instanceof Error) {
+      res.status(500).send(e.message);
+    }
+  }
+});
 
-// itemsRouter.get("/",async)
+
 
 // POST items
 
+itemsRouter.get("/",async(req: Request, res: Response) => {
+  try {
+    const item: BaseItem = req.body;
+    const newItem = await ItemService.create(item);
+    res.status(201).json(newItem); 
+  }
+  catch (e: unknown) {
+    if (e instanceof Error) {
+      res.status(500).send(e.message);
+    }
+  }
+});
+
+
 // PUT items/:id
 
+itemsRouter.put("/:id", async(req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id, 10);
+  try {
+    const itemUpdate: Item = req.body;
+    const existingItem: Item = await ItemService.find(id);
+
+    if (existingItem) {
+      const updatedItem = await ItemService.update(id, itemUpdate);
+      return res.status(200).json(updatedItem);
+    }
+    const newItem = await ItemService.create(itemUpdate);
+    return res.status(201).json();
+
+  }
+
+  catch (e: unknown) {
+    if (e instanceof Error) {
+      res.status(500).send(e.message);
+    }
+  }
+});
+
+
+
 // DELETE items/:id
+
+itemsRouter.delete("/:id", async (req: Request, res: Response) => {
+try {
+const id: number = parseInt(req.params.id, 10);
+await ItemService.remove(id);
+res.sendStatus(204);
+}
+catch (e: unknown) {
+  if (e instanceof Error) {
+   return res.status(500).send(e.message);
+  }
+}
+
+});
